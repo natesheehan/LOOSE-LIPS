82dc36608e1a9aa326f5278c95d45bfae1e3d9f1
natesheehan <nathanaelsheehan@gmail.com>
2023-01-10 17:06:54 +0000

    update site with new homepage layout

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,21 @@
 import '../design/style.css'
 
-const userCardTemplate = document.querySelector("[data-user-template]")
-const userCardContainer = document.querySelector("[data-user-cards-container]")
-const searchInput = document.querySelector("[data-search]")
+// const searchInput = document.querySelector("[data-search]")
 
 let users = []
 
-searchInput.addEventListener("input", e => {
-  const value = e.target.value.toLowerCase()
-  users.forEach(user => {
-    const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
-    user.element.classList.toggle("hide", !isVisible)
-  })
-})
+// searchInput.addEventListener("input", e => {
+//   const value = e.target.value.toLowerCase()
+//   users.forEach(user => {
+//     const isVisible =
+//       user.name.toLowerCase().includes(value) ||
+//       user.email.toLowerCase().includes(value)
+//     user.element.classList.toggle("hide", !isVisible)
+//   })
+// })
+
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
 
 fetch("../data/data.json")
   .then(res => res.json())
@@ -25,23 +26,62 @@ fetch("../data/data.json")
       const body = card.querySelector("[data-body]")
       const img = card.querySelector("[data-img]")
       const text = card.querySelector("[data-text]")
+      const label = card.querySelector("[data-label]")
 
       header.textContent = user.name
       body.textContent = user.date
       img.src = user.img
       text.textContent = user.text
+      label.textContent = user.labels
 
 
       userCardContainer.append(card)
-      
+
       return {
         name: user.name,
         email: user.date,
         text: user.text,
+        label: user.labels,
         element: card
       }
     })
   })
+
+
+const eventCardTemplate = document.querySelector("[data-events-template")
+const eventCardContainer = document.querySelector("[data-event-cards-container]")
+
+fetch("../data/events.json")
+  .then(res => res.json())
+  .then(events => {
+    events = events.map(event => {
+      const event_card = eventCardTemplate.content.cloneNode(true).children[0]
+      
+      const e_name = event_card.querySelector("[data-eventname]")
+      const e_date = event_card.querySelector("[data-eventdate]")
+      const e_artist = event_card.querySelector("[data-eventartist]")
+      const e_location = event_card.querySelector("[data-eventlocation]")
+      const e_img = event_card.querySelector("[data-eventimg]")
+
+      e_name.textContent = event.name
+      e_date.textContent = event.date
+      e_artist.textContent = event.artists
+      e_location.textContent = event.location
+      e_img.src = event.img
+
+
+      eventCardContainer.append(event_card)
+
+      return {
+        name: event.name,
+        date: event.date,
+        artists: event.artists,
+        location: event.location,
+        element: event_card
+      }
+    })
+  })
+
 
 
 function waitForElm(selector) {
@@ -79,15 +119,3 @@ waitForElm('#btn').then((elm) => {
     }
   });
 });
-
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-let video = document.getElementById("video")
-let playButton = document.querySelector("play-button")
